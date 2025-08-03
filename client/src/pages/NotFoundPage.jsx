04544c2d0a0528f96b427bc2e89ceec603d7e489
MarkCryX <144240468+MarkCryX@users.noreply.github.com>
2025-08-03 14:31:23 +0700

    style: improve ui

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,9 +1,8 @@
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center text-center">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-7xl font-bold text-gray-300">404 ERROR</h1>
       <h2 className="mt-6 text-3xl font-bold">Page Not Found</h2>
-      <p className="text-xl">The page are you lookinh for does not exist.</p>
     </div>
   );
 };
